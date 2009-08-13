53cf81b5b268fe040d49bdeaa8fa1ec38aab141e
Joseph HALTER <joseph.halter@akiraweb.lu>
2009-08-13 11:58:17 +0200

    Add validate_numeric_matcher and specs

--- lib/rspec_sequel/matchers/validate_numeric.rb
+++ lib/rspec_sequel/matchers/validate_numeric.rb
@@ -1,20 +1,20 @@
 module RspecSequel
   module Matchers
 
-    class ValidateIntegerMatcher < RspecSequel::Validation
+    class ValidateNumericMatcher < RspecSequel::Validation
       def description
-        desc = "validate that #{@attribute.inspect} is a valid integer"
+        desc = "validate that #{@attribute.inspect} is a valid float"
         desc << " with option(s) #{hash_to_nice_string @options}" unless @options.empty?
         desc
       end
 
       def validation_type
-        :validates_integer
+        :validates_numeric
       end
     end
 
-    def validate_integer(*args)
-      ValidateIntegerMatcher.new(*args)
+    def validate_numeric(*args)
+      ValidateNumericMatcher.new(*args)
     end
 
   end
--- spec/validate_numeric_matcher_spec.rb
+++ spec/validate_numeric_matcher_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
-describe "validate_integer_matcher" do
+describe "validate_numeric_matcher" do
 
   before :all do
     define_model :item do
       plugin :validation_helpers
       def validate
-        validates_integer [:id, :name], :allow_nil => true
+        validates_numeric [:id, :name], :allow_nil => true
       end
     end
   end
@@ -16,12 +16,12 @@ describe "validate_integer_matcher" do
   describe "arguments" do
     it "should require attribute" do
       lambda{
-        @matcher = validate_integer
+        @matcher = validate_numeric
       }.should raise_error(ArgumentError)
     end
     it "should refuse additionnal parameters" do
       lambda{
-        @matcher = validate_integer :name, :id
+        @matcher = validate_numeric :name, :id
       }.should raise_error(ArgumentError)
     end
   end
@@ -29,11 +29,11 @@ describe "validate_integer_matcher" do
   describe "messages" do
     describe "without option" do
       it "should contain a description" do
-        @matcher = validate_integer :name
-        @matcher.description.should == "validate that :name is a valid integer"
+        @matcher = validate_numeric :name
+        @matcher.description.should == "validate that :name is a valid float"
       end
       it "should set failure messages" do
-        @matcher = validate_integer :name
+        @matcher = validate_numeric :name
         @matcher.matches? subject
         @matcher.failure_message.should == "expected Item to " + @matcher.description
         @matcher.negative_failure_message.should == "expected Item to not " + @matcher.description
@@ -41,24 +41,24 @@ describe "validate_integer_matcher" do
     end
     describe "with options" do
       it "should contain a description" do
-        @matcher = validate_integer :name, :allow_nil => true
-        @matcher.description.should == "validate that :name is a valid integer with option(s) :allow_nil => true"
+        @matcher = validate_numeric :name, :allow_nil => true
+        @matcher.description.should == "validate that :name is a valid float with option(s) :allow_nil => true"
       end
       it "should set failure messages" do
-        @matcher = validate_integer :price, :allow_nil => true
+        @matcher = validate_numeric :price, :allow_nil => true
         @matcher.matches? subject
         @matcher.failure_message.should == "expected Item to " + @matcher.description
         @matcher.negative_failure_message.should == "expected Item to not " + @matcher.description
       end
       it "should explicit used options if different than expected" do
-        @matcher = validate_integer :name, :allow_blank => true
+        @matcher = validate_numeric :name, :allow_blank => true
         @matcher.matches? subject
         explanation = " but called with option(s) :allow_nil => true instead"
         @matcher.failure_message.should == "expected Item to " + @matcher.description + explanation
         @matcher.negative_failure_message.should == "expected Item to not " + @matcher.description + explanation
       end
       it "should warn if invalid options are used" do
-        @matcher = validate_integer :name, :allow_anything => true
+        @matcher = validate_numeric :name, :allow_anything => true
         @matcher.matches? subject
         explanation = " but option :allow_anything is not valid"
         @matcher.failure_message.should == "expected Item to " + @matcher.description + explanation
@@ -68,10 +68,10 @@ describe "validate_integer_matcher" do
   end
 
   describe "matchers" do
-    it{ should validate_integer(:name) }
-    it{ should validate_integer(:name, :allow_nil => true) }
-    it{ should_not validate_integer(:price) }
-    it{ should_not validate_integer(:name, :allow_blank => true) }
+    it{ should validate_numeric(:name) }
+    it{ should validate_numeric(:name, :allow_nil => true) }
+    it{ should_not validate_numeric(:price) }
+    it{ should_not validate_numeric(:name, :allow_blank => true) }
   end
 
 end
