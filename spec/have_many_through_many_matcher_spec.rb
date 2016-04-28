c53d235b53dd4f4deba1ea26ead01b00b2043960
Hunter Stewart <hetre85@gmail.com>
2016-04-28 16:08:44 -0500

    Support many_through_many
    This commit adds support for testing many_through_many associations
    along with a spec to test the behavior.

---
+++ spec/have_many_through_many_matcher_spec.rb
@@ -0,0 +1,56 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe "have_many_through_many_matcher" do
+
+  before do
+    define_model :item
+    define_model :comment do
+      plugin :many_through_many
+      many_through_many :items, [[:items_comments, :item_id, :comment_id]]
+    end
+  end
+
+  subject{ Comment }
+
+  describe "messages" do
+    describe "without option" do
+      it "should contain a description" do
+        @matcher = have_many_through_many :items
+        expect( @matcher.description ).to eq "have a many_through_many association :items"
+      end
+      it "should set failure messages" do
+        @matcher = have_many_through_many :items
+        @matcher.matches? subject
+        expect( @matcher.failure_message ).to eq "expected Comment to " + @matcher.description
+        expect( @matcher.failure_message_when_negated ).to eq "expected Comment to not " + @matcher.description
+      end
+    end
+    describe "with options" do
+      it "should contain a description" do
+        @matcher = have_many_through_many :items, :class_name => "Item"
+        expect( @matcher.description ).to eq 'have a many_through_many association :items with option(s) :class_name => "Item"'
+      end
+      it "should set failure messages" do
+        @matcher = have_many_through_many :items, :class_name => "Item"
+        @matcher.matches? subject
+        expect( @matcher.failure_message ).to eq "expected Comment to " + @matcher.description
+        expect( @matcher.failure_message_when_negated ).to eq "expected Comment to not " + @matcher.description
+      end
+      it "should explicit used options if different than expected" do
+        @matcher = have_many_through_many :items, :class_name => "Price"
+        @matcher.matches? subject
+        explanation = ' expected :class_name == "Price" but found "Item" instead'
+        expect( @matcher.failure_message ).to eq "expected Comment to " + @matcher.description + explanation
+        expect( @matcher.failure_message_when_negated ).to eq "expected Comment to not " + @matcher.description + explanation
+      end
+    end
+  end
+
+  describe "matchers" do
+    it{ is_expected.to have_many_through_many(:items) }
+    it{ is_expected.not_to have_many_through_many(:prices) }
+    it{ is_expected.not_to have_many_through_many(:items, :class_name => "Price") }
+    it{ is_expected.not_to have_many_through_many(:items, :join_table => :items_comments) }
+  end
+
+end
